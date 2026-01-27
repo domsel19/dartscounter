9d7984e35b9197b4d75e6a55f7d89a0ba4f87bf2
domsel19 <dominiksalge@outlook.de>
2026-01-27 16:43:38 +0100

    Basic imports included.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,18 @@
-package com.example;
+package com.dartscounter;
 
-/**
- * Hello world!
- */
+import java.sql.DriverManager;
+import java.sql.Conncetion;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import java.util.Date;
+
+public class App extends JFrame {
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+}
 }
